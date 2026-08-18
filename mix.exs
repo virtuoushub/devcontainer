f7d5abe6bc69a2f91c7b2b539371f330c4d984e9
Pete <petercolapietro+github@gmail.com>
2026-08-18 15:54:27 -0700

    feat: add popcorn dependency
    Bring in Popcorn (WASM runtime for Elixir), matching the
    Popcorn-compatible Elixir/OTP versions already pinned for this
    devcontainer.

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule Workspace.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:popcorn, "~> 0.3.3"}
     ]
   end
 
